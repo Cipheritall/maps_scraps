472ba9f97e06894de6813fb18e7e7d8aebc817ae
Lah <rbk@hada>
2022-08-07 23:28:22 +0200

    go

---
+++ config.py
@@ -0,0 +1,25 @@
+# Maps api key
+api_key = "AIzaSyB8TweCbt1tm2N9TnRmhx6bMN5wguIFVAk"
+
+# Constants
+khribga_high_left = ["32.907227","-6.957063"]
+jadida_high_left = ["33.263203", "-8.541662"]
+berkane_hight_left = ["34.950850", "-2.336993"]
+
+
+# Height and Width
+city_height = 5000 
+city_width = 5000
+# Rayon --> multip
+multip = 100 # 1111 m
+# multip = 10000 # 11 m
+# multip = 1000 # 111 m
+# multip = 100 # 1111 m 
+# multip = 10 # 11 km
+
+# Variables
+city_name = "Berkane"
+city_high_left = berkane_hight_left
+business_type = "establishment"
+keyword = ""
+business_total = 60
--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 # Definitions
-import requests
 import pandas as pd 
-import json
-import datetime
-from time import sleep
-import numpy as np
+from bokeh.io import show
+from bokeh.plotting import gmap
+from bokeh.models import GMapOptions
+from bokeh.models import ColumnDataSource
+from bokeh.io import output_notebook
+
+from bokeh.plotting import figure, output_file, save
+
+
 
 from gmapi import * 
 from config import *
 from centers import *
 
 
-# Constants
-khribga_high_left = ["32.907227","-6.957063"]
-jadida_high_left = ["33.263203", "-8.541662"]
-berkane_hight_left = ["34.950850", "-2.336993"]
-
-
-# Height and Width
-city_height = 5000 
-city_width = 5000
-multip = 100 # 1111 m
-# multip = 10000 # 11 m
-# multip = 1000 # 111 m
-# multip = 100 # 1111 m 
-# multip = 10 # 11 km
-
-# Variables
-city_name = "Berkane"
-city_high_left = berkane_hight_left
-business_type = "establishment"
-keyword = ""
-business_total = 60
 
 # Processing Centers
 city_exterms = get_extermes(city_high_left,city_height,city_width)
 centers = get_centers(city_exterms[0],city_exterms[1])
 centers
-pd.DataFrame(centers).to_csv("centers.csv")
+pd.DataFrame(centers).to_csv("output/centers.csv")
 
-if(input("Are you ok with the cucrrent coverage y/N : ")=="Y"):
-    print(ok)
+
+bokeh_centers = {}
+bokeh_centers["lon"] = []
+bokeh_centers["lat"] = []
+
+for pos in centers:
+    bokeh_centers["lat"].append(float(pos[0])) 
+    bokeh_centers["lon"].append(float(pos[1])) 
+df_bokeh_centers = pd.DataFrame(bokeh_centers)
+
+
+# set output to static HTML file
+output_file(filename="output/centers.html", title="Static HTML file")
+
+#output_notebook()
+bokeh_width, bokeh_height = 1000,1000
+
+lat, lon = float(city_high_left[0]),float(city_high_left[1])
+
+
+def plot(lat, lng, zoom=10, map_type='terrain'):
+    gmap_options = GMapOptions(lat=lat, lng=lng, 
+                               map_type=map_type, zoom=zoom)
+    p = gmap(api_key, gmap_options, title='Pays de Gex', 
+             width=bokeh_width, height=bokeh_height)
+    df_bokeh_centers["radius"]=1
+    source = ColumnDataSource(
+        data=dict(lon=bokeh_centers["lon"],
+                  lat=bokeh_centers["lat"])
+    )
+    # see how we specify the x and y columns as strings, 
+    # and how to declare as a source the ColumnDataSource:
+    center = p.circle('lon', 'lat', size=10, alpha=0.6, 
+                      color='red', source=source)
+    show(p)
+    return p
+
+p = plot(lat, lon)
+
+if(input("Are you ok with the cucrrent coverage y/N : ") in ["Y","y","yes","oui"]):
+    print("ok")
 else:
     print("aborted")
